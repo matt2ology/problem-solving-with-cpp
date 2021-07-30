c6224a782b9394f9ceda92b8975a08cfc96e68ed
matt2ology <34324060+matt2ology@users.noreply.github.com>
2021-07-30 02:03:57 -0700

    init commit - bulding up to a solution

---
+++ c03p05-more-flow-of-control/LongDistantCallCalculator.cpp
@@ -0,0 +1,27 @@
+//File Name: LongDistantCallCalculator.cpp
+//Author: matt2ology
+//Description: Program that computes the cost of a long-distance call.
+//Last Changed: July 30, 2021
+//Textbook page 175, PDF page 207
+
+#include <iostream>
+// Header that defines the standard input/output stream objects
+#include <string>
+// Include the string library
+
+int main()
+{
+    std::string twenty_four_hour_input = "";
+    std::cout << "Enter 24-hour notation: ";
+    std::scanf("%1i", twenty_four_hour_input);
+    std::cout << "You have entered: " << twenty_four_hour_input << std::endl;
+
+    
+
+    system("pause");
+    EXIT_SUCCESS; // return 0;
+}
+
+
+
+
