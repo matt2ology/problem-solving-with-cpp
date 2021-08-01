d0f22397efbc751a0bdbbfcd548a1a4a5a2f6192
matt2ology <34324060+matt2ology@users.noreply.github.com>
2021-07-31 21:38:08 -0700

    Fixed day of week input and readout

--- c03p05-more-flow-of-control/LongDistantCallCalculator.cpp
+++ c03p05-more-flow-of-control/LongDistantCallCalculator.cpp
@@ -11,17 +11,33 @@
 
 int main()
 {
-    std::string twenty_four_hour_input = "";
-    std::cout << "Enter 24-hour notation: ";
-    std::scanf("%1i", twenty_four_hour_input);
-    std::cout << "You have entered: " << twenty_four_hour_input << std::endl;
+    char day_first_char, day_second_char;
+    std::string twenty_four_hour_input;
+    int call_duration;
 
-    
+    std::cout << "Welcome to Horizon's long-distance call calculator" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "What day of week did you make your call? (Mo Tu We Th Fr Sa Su)" << std::endl;
+    std::cout << "Day of week: ";
+    std::cin >> day_first_char >> day_second_char;
+    std::cout << std::endl;
+
+    std::cout << "What time was this call held? Enter in 24-hour format (hh:mm)" << std::endl;
+    std::cout << "Time the call started: ";
+    std::cin >> twenty_four_hour_input;
+    std::cout << std::endl;
+
+    std::cout << "What was the duration of this call in minutes? (2 hours = 120 minutes)" << std::endl;
+    std::cout << "Call duration in minutes: ";
+    std::cin >> call_duration;
+    std::cout << std::endl;
+
+    std::cout << "You have entered the following:" << std::endl;
+    std::cout << "Day of week: " << day_first_char << day_second_char << std::endl;
+    std::cout << "Time the call started: " << twenty_four_hour_input << std::endl;
+    std::cout << "Call duration in minutes: " << call_duration << std::endl;
 
     system("pause");
     EXIT_SUCCESS; // return 0;
 }
-
-
-
-
